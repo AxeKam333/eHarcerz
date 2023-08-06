95e220e04a3eba6f87326768bda49e3fba6f92c8
AxeKam333 <olek.kaminski5@gmail.com>
2023-08-06 12:42:55 +0200

    initial commit

---
+++ stopnie_i_sprawnosci/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for stopnie_i_sprawnosci project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stopnie_i_sprawnosci.settings')
+
+application = get_asgi_application()
