688a191c22a2525f186584fb97038780e880ce37
AxeKam333 <olek.kaminski5@gmail.com>
2023-08-06 14:31:40 +0200

    models

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Categories, Spec, Badge, Troop, Patrol, Scout
 
-# Register your models here.
+
+admin.site.register(Categories)
+admin.site.register(Spec)
+admin.site.register(Badge)
+admin.site.register(Troop)
+admin.site.register(Patrol)
+admin.site.register(Scout)
+
+admin.site_header = 'Stopnie i Sprawności'
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+# hierarchy of badges:
+
+class Categories(models.Model):
+    name = models.CharField(max_length=100)
+    #__str__ = str(name)
+
+
+class Spec(models.Model):
+    name = models.CharField(max_length=100)
+    
+    category = models.ForeignKey(to=Categories, on_delete=models.DO_NOTHING, default=None, blank=True)
+
+
+class Badge(models.Model):
+    name = models.CharField(max_length=100)
+    stars = models.IntegerField(default=0)
+
+    spec = models.ForeignKey(to=Spec, on_delete=models.DO_NOTHING, default=None, blank=True)
+
+
+# hierarchy of scouts:
+
+class Troop(models.Model):
+    name = models.CharField(max_length=100)
+    number = models.IntegerField(default=0)
+    patron = models.CharField(max_length=100, default=None, blank=True)
+
+class Patrol(models.Model):
+    name = models.CharField(max_length=100)
+    leader = models.CharField(max_length=100)
+
+    troop = models.ForeignKey(to=Troop, on_delete=models.DO_NOTHING)
+
+
+class Scout(models.Model):
+    first_name = models.CharField(max_length=100,default='Imie')
+    last_name = models.CharField(max_length=100,default=None, blank=True)
+
+    patrol = models.ForeignKey(to=Patrol, on_delete=models.DO_NOTHING, default=None, blank=True)
+
+    RANK_NAMES = (
+        ('BIS', 'BISZKOPT'),
+        ('DH', 'DRUH'),
+        ('MŁ', 'MŁODZIK'),
+        ('WYW', 'WYWIADOWCA'),
+        ('ĆW', 'ĆWIK'),
+        ('HO', 'HARCERZ ORLI'),
+        ('HR', 'HARCERZ RZECZYPOSPOLITEJ'),
+    )
+    rank = models.CharField(max_length=100, choices=RANK_NAMES, default='BISZ')
+
+    badges = models.ManyToManyField(to=Badge, blank=True, default=None)
